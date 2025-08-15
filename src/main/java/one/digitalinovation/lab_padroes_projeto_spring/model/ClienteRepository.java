311c16427dc902b69f57f0713cb75d97f1826283
ananias <netim.cunha@gmail.com>
2025-08-14 21:09:55 -0300

    criaçao dos modelos

---
+++ src/main/java/one/digitalinovation/lab_padroes_projeto_spring/model/ClienteRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinovation.lab_padroes_projeto_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+    
+}
---
+++ src/main/java/one/digitalinovation/lab_padroes_projeto_spring/model/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinovation.lab_padroes_projeto_spring.model;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
+    
+}
+   
